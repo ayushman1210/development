7b3a69681aa9608a7517b9e3714c93a6ae00ffff
ayushman1210 <ayushmangupta892@gmail.com>
2025-05-22 17:30:39 +0530

    added code

---
+++ node/mogodb/db.js
@@ -0,0 +1,27 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const objectId=mongoose.ObjectId;
+
+
+const User= new Schema({
+    name:String,
+    email:{
+        unique:true,
+        type:String
+    },
+    password:String
+});
+
+const Todo=new Schema({
+    userId:objectId,
+    Title:String,
+    password:String
+});
+
+const userModel=mongoose.model('user',User);
+const todomodel=mongoose.model('todos',Todo);
+
+
+module.exports={
+    userModel,todomodel
+}
--- node/mogodb/index.js
+++ node/mogodb/index.js
@@ -1,10 +1,60 @@
 const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
+const {userModel,todomodel}=require('./db');
+const jwt=require('jsonwebtoken')
+const JWT_SECRET="manloveyani"
+let db="mongodb://localhost:27017/todo";
+
+app.use(express.json())
 app.get('/',(req,res)=>{
     res.send('hello welcome to mongodb ')
 })
-let db="mongodb://localhost:27017";
+
+app.post('/signin',async(req,res)=>{
+const {email,password,name}=req.body 
+
+ await userModel.create({
+    email:email,
+    password:password,
+    name:name,
+})
+
+
+res.json({
+    success:true,
+    message:"your account has been created"
+})
+})
+
+
+
+
+app.post('/signup',async(req,res)=>{
+    const {email,password}=req.body 
+   const user=await userModel.findOne({email:email,password:password})
+    if(user){
+        const token=jwt.sign({
+            id:user._id
+        },   JWT_SECRET);
+        res.json({
+            token:token
+        })
+    }
+    else{
+        res.json({
+            message:"unauthenticated user "
+        })
+    }
+})
+
+app.post('/todo',(req,res)=>{
+
+})
+
+app.get('todos',(req,res)=>{
+    
+})
 
 app.listen(3000,async()=>{
     console.log("server started")
