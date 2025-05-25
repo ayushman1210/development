afd31cac485a80c8c5809d7ba9c1f171288541ea
ayushman1210 <ayushmangupta892@gmail.com>
2025-05-25 22:46:03 +0530

    learned about input validation zod library

--- node/mogodb/index.js
+++ node/mogodb/index.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const app=express();
+const bcrypt=require('bcrypt')
 const mongoose=require('mongoose');
 const {userModel,todomodel}=require('./db');
 const jwt=require('jsonwebtoken')
+const {z}=require('zod')
 const JWT_SECRET="manloveyani"
 let db="mongodb://localhost:27017/todo";
 
@@ -17,7 +19,7 @@ const decodedata=jwt.verify(token,JWT_SECRET)
 if(decodedata){
     req.userId=decodedata.userId;
     console.log(decodedata.userId)
-    next();
+    next(); 
 }
 else{
     res.json({
@@ -30,17 +32,36 @@ else{
 app.post('/signin',async(req,res)=>{
 const {email,password,name}=req.body 
 
- await userModel.create({
-    email:email,
-    password:password,
-    name:name,
+const required=z.object({
+    email:z.string().max(100).min(8).email(),
+    name:z.string().max(100).min(8),
+    password:z.string().max(100).min(8)
 })
 
+const parsedata=required.safeParse(req.body);
+if(!parsedata.success){
+    res.json({
+        message:'incorrect format',
+        error:parsedata.error
+    })
+    return 
+}
+const hashpassword= await bcrypt.hash(password,5);
+
+ await userModel.create({
+    email:email,
+    password:hashpassword,
+    name:name,
+})
+    res.json({
+        message:"error"
+    })
 
 res.json({
     success:true,
     message:"your account has been created"
 })
+
 })
 
 
@@ -48,9 +69,16 @@ res.json({
 
 app.post('/signup',async(req,res)=>{
     const {email,password}=req.body 
-   const user=await userModel.findOne({email:email,password:password})
+   const user=await userModel.findOne({email:email})
 //    console.log(user._id);
-    if(user){
+if(!user){
+    res.status(403).json({
+        message:"not registered email "
+    })
+}
+   const match= await bcrypt.compare(password,user.password)
+
+    if(match){
         const token=jwt.sign({
             userId:user._id.toString()
         },   JWT_SECRET);
